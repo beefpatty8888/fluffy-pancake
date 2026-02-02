c0c0577eb0a8a2d32a8233dd340ceafb93b1493b
jack <16585824+beefpatty8888@users.noreply.github.com>
2026-02-01 19:12:50 -0600

    Add additional test prompts and resulting image inferences.

--- flux2.py
+++ flux2.py
@@ -9,20 +9,26 @@ image_filename="flux2_image-"+date+".png"
 
 pipeline = Flux2Pipeline.from_pretrained("black-forest-labs/FLUX.2-dev", torch_dtype=torch.bfloat16, device_map="balanced")
 
-#pipeline = DiffusionPipeline.from_pretrained(
- ##"black-forest-labs/FLUX.2-dev", torch_dtype=torch.bfloat16, device_map="balanced"
-#  "black-forest-labs/FLUX.2-klein-9B", torch_dtype=torch.bfloat16, device_map="balanced"
-#)
-
 #prompt = """
 #a cat with a cowboy hat and bandana sipping whiskey in the hills of Texas with a forest in the background. Sunny skies, clouds in the horizon
 #epic, gorgeous, film grain
 #"""
 
+#prompt = """
+#a woman with a sword and a shield is riding a horse on a trail in the woods. Ahead of her in the distance is a dragon guarding a castle. Rays of sunshine is shining through the clouds. In the background are some hills and a waterfall.
+#epic, adventurous.
+#"""
+
+#prompt = """
+#A silhouette of a couple dancing the tango under a pavilion at night. The moon is full and the moonlight is reflecting on a lake in the background.
+#The pavilion is decorated with lanterns, creating a romantic atmosphere. The park is filled with trees, flowers, benches and a cobblestone pathway.
+#"""
+
 prompt = """
-a woman with a sword and a shield is riding a horse on a trail in the woods. Ahead of her in the distance is a dragon guarding a castle. Rays of sunshine is shining through the clouds. In the background are some hills and a waterfall.
-epic, adventurous.
+a cowboy is riding a horse through the prairie at sunrise, herding cattle and longhorn bovine. 
+In the background are some hills and a river reflecting the golden light of the rising sun with a few scattered clouds.
 """
+
 pipeline.reset_device_map()
 #pipeline.enable_model_cpu_offload()
 pipeline.enable_sequential_cpu_offload()
--- qwen-2512.py
+++ qwen-2512.py
@@ -14,10 +14,19 @@ pipeline = DiffusionPipeline.from_pretrained(
 #a rocket with boosters, one on each side, is sitting on a launchpad at night. Searchlights illuminate the rocket. A full moon is up, and the moonlight is reflected on the lake in the background.
 #"""
 
-prompt = """
-a woman with a sword and a shield is riding a horse on a trail in the woods. Ahead of her in the distance is a dragon guarding a castle. Rays of sunshine is shining through the clouds. In the background are some hills and a waterfall.
-"""
+#prompt = """
+#a woman with a sword and a shield is riding a horse on a trail in the woods. Ahead of her in the distance is a dragon guarding a castle. Rays of sunshine is shining through the clouds. In the background are some hills and a waterfall.
+#"""
 
+#prompt = """
+#A silhouette of a couple dancing the tango under a pavilion at night. The moon is full and the moonlight is reflecting on a lake in the background.
+#The pavilion is decorated with lanterns, creating a romantic atmosphere. The park is filled with trees, flowers, benches and a cobblestone pathway.
+#"""
+
+prompt = """
+a cowboy is riding a horse through the prairie at sunrise, herding cattle and longhorn bovine. 
+In the background are some hills and a river reflecting the golden light of the rising sun with a few scattered clouds.
+"""
 
 pipeline.reset_device_map()
 #pipeline.enable_model_cpu_offload()
